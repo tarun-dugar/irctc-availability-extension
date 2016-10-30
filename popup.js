2f198582f921025caa7ef6a7704092a3993f22c7
Tarun Dugar <tarundugar1992@gmail.com>
2016-10-30 18:01:24 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var availabilityButton = document.getElementById('checkAvailability');
+  availabilityButton.addEventListener('click', function() {
+    console.log('hello');
+  });
+});
